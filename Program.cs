3bdbc207cc2a17b35c0e4d11800008ac66e8670b
robbfreitas <robertocompromissos@gmail.com>
2022-12-08 19:32:33 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Course;
+using System.Globalization;
+
+namespace SistemaDeEstoquePOOcomCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 2);
+
+            
+
+            Console.WriteLine(p);
+        }
+    }
+}
