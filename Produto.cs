b44611bb6dd1ab3a18119816cb837d09598641f8
robbfreitas <robertocompromissos@gmail.com>
2022-12-09 22:06:21 -0300

    Refatoração: inseridas auto properties

--- Produto.cs
+++ Produto.cs
@@ -4,8 +4,8 @@ namespace Course
     class Produto
     {
         private string _nome;
-        private double _preco;
-        private int _quantidade;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
 
         public Produto()
         {
@@ -14,8 +14,8 @@ namespace Course
         public Produto(string nome, double preco, int quantidade)
         {
             _nome = nome;
-            _preco = preco;
-            _quantidade = quantidade;
+            Preco = preco;
+            Quantidade = quantidade;
         }
 
         public string Nome
@@ -30,34 +30,24 @@ namespace Course
             }
         }
 
-        public double Preco
-        {
-            get { return _preco; }
-        }
-
-        public int Quantidade
-        {
-            get { return _quantidade; }
-        }
-
         public double ValorTotalEmEstoque()
         {
-            return _preco * _quantidade;
+            return Preco * Quantidade;
         }
         public void AdicionarProdutos(int quantidade)
         {
-            _quantidade += quantidade;
+            Quantidade += quantidade;
         }
         public void RemoverProdutos(int quantidade)
         {
-            _quantidade -= quantidade;
+            Quantidade -= quantidade;
         }
         public override string ToString()
         {
             return _nome 
                 + ", $ " 
-                + _preco.ToString("F2", CultureInfo.InvariantCulture) 
-                + ", " + _quantidade 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", " + Quantidade 
                 + " unidades, Total: $ " 
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
